3a0941a662c58ac622515a84827ce5448271dadc
Crowdbotics <engineering@crowdbotics.com>
2023-03-27 00:21:25 +0000

    Initial commit.

---
+++ backend/restless_hall_dev_72238/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for restless_hall_dev_72238 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restless_hall_dev_72238.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-hall-dev-72238",
+  url: "https://restless-hall-dev-72238.botics.co",
+  api: "https://restless-hall-dev-72238.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
